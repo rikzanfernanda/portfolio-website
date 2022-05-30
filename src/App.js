f9eca738e186d35bdba9bf6514aff40a4f3455fe
rikzan fernanda <fernandarikzan@gmail.com>
2022-05-30 08:55:27 +0700

    chore: add scroll restoration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes, useLocation} from "react-router-dom";
 import Home from "./pages/Home";
 import NoPage from "./pages/NoPage";
 import Web from "./pages/Web";
 import Editing from "./pages/Editing";
+import {useLayoutEffect} from "react";
+
+const Wrapper = ({children}) => {
+    const location = useLocation();
+    useLayoutEffect(() => {
+        document.documentElement.scrollTo(0, 0);
+    }, [location.pathname])
+    return children;
+}
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />}/>
-            <Route path='/web' element={<Web />} />
-            <Route path='/editing' element={<Editing />} />
-            <Route path='*' element={<NoPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Wrapper>
+                    <Routes>
+                        <Route path='/' element={<Home/>}/>
+                        <Route path='/web' element={<Web/>}/>
+                        <Route path='/editing' element={<Editing/>}/>
+                        <Route path='*' element={<NoPage/>}/>
+                    </Routes>
+                </Wrapper>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
