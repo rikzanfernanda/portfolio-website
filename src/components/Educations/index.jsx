7954dd9a544e69dc09c0de51bd209d996b74e3df
rikzan fernanda <fernandarikzan@gmail.com>
2023-06-05 14:45:29 +0700

    education: set container and update layout

--- src/components/Educations/index.jsx
+++ src/components/Educations/index.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading } from '@chakra-ui/react'
+import { Box, Container, Flex, Heading } from '@chakra-ui/react'
 import React from 'react'
 import education from '../../constants/education'
 import EducationCard from './components/EducationCard'
@@ -6,47 +6,44 @@ import EducationCard from './components/EducationCard'
 const Educations = () => {
     return (
         <Box
-            pt={{
-                base: 4,
+            py={{
+                base: 6,
                 md: 16,
                 lg: 24
             }}
-            px={{
-                base: 3,
-                md: '10rem'
-            }}
+            backgroundColor={'gray.100'}
         >
-            <Heading
-                fontSize={{
-                    base: '.85rem',
-                    md: '1.4rem',
-                    lg: '1.8rem',
-                    xl: '2.5rem'
-                }}
-                textAlign={{
-                    base: 'left',
-                    md: 'center'
-                }}
-                mb={{
-                    base: 3,
-                    md: '2rem',
-                    lg: '3rem'
-                }}
+            <Container
+                maxW={'container.xl'}
             >
-                {education.title}
-            </Heading>
+                <Heading
+                    fontSize={{
+                        base: '.85rem',
+                        md: '2rem',
+                    }}
+                    textAlign={{
+                        base: 'left',
+                    }}
+                    mb={{
+                        base: 3,
+                        md: '1.25rem',
+                    }}
+                >
+                    {education.title}
+                </Heading>
 
-            <Flex
-                justifyContent={'center'}
-                alignItems={'center'}
-                direction={{
-                    base: 'column'
-                }}
-            >
-                {education.data.map((item, i) => {
-                    return <EducationCard key={i} item={item} />
-                })}
-            </Flex>
+                <Flex
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                    direction={{
+                        base: 'column'
+                    }}
+                >
+                    {education.data.map((item, i) => {
+                        return <EducationCard key={i} item={item} />
+                    })}
+                </Flex>
+            </Container>
         </Box>
     )
 }
