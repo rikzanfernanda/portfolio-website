ba3873c02b8b1c801f6f7043b4de56ad1f29b278
rikzan fernanda <fernandarikzan@gmail.com>
2023-06-05 14:33:13 +0700

    hero: set container

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { Box, Image, Text } from '@chakra-ui/react'
+import { Box, Container, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import HeroImage from '../assets/images/bg-jumbotron.jpg'
 import ABOUT from '../constants/about'
@@ -8,11 +8,6 @@ const Hero = () => {
         <Box
             bgColor={'blackAlpha.50'}
             h={'100vh'}
-            p={{
-                base: 6,
-                md: 10,
-                lg: '10rem'
-            }}
             bgImg={HeroImage}
             bgPos={'50% 100%'}
             bgRepeat={'no-repeat'}
@@ -23,63 +18,72 @@ const Hero = () => {
             color={'white'}
             flexDirection={'column'}
         >
-            <Image
-                src={ABOUT.profile}
-                alt={'profile'}
-                borderRadius={'full'}
-                boxSize={{
-                    base: 100,
-                    md: 150,
-                    lg: 200
-                }}
-                mb={{
-                    base: 4
-                }}
-                bgSize={'cover'}
-                objectFit={'cover'}
-            />
-            <Box
-                textAlign={'center'}
-                mb={{
-                    base: 8,
-                    md: 12,
-                    lg: 14
-                }}
+            <Container
+                maxW={'container.xl'}
             >
-                <Text
-                    fontSize={{
-                        base: '3xl',
-                        md: '6xl',
-                        lg: '7xl'
-                    }}
-                    fontWeight={'bold'}
+                <Box
+                    textAlign={'center'}
                 >
-                    {ABOUT.name}
-                </Text>
-                <Text
-                    fontSize={{
-                        base: 'md',
-                        md: '1rem',
-                        lg: '1rem',
-                        xl: '1.25rem'
+                    <Image
+                        src={ABOUT.profile}
+                        alt={'profile'}
+                        borderRadius={'full'}
+                        boxSize={{
+                            base: 100,
+                            md: 150,
+                            lg: 200
+                        }}
+                        mb={{
+                            base: 4
+                        }}
+                        bgSize={'cover'}
+                        objectFit={'cover'}
+                        marginX={'auto'}
+                    />
+                </Box>
+                <Box
+                    textAlign={'center'}
+                    mb={{
+                        base: 8,
+                        md: 12,
+                        lg: 14
                     }}
                 >
-                    {ABOUT.note}
-                </Text>
-            </Box>
-            <Box textAlign={'center'}>
-                <Text
-                    fontSize={{
-                        base: 'md',
-                        md: '1rem',
-                        lg: '1rem',
-                        xl: '1.25rem'
-                    }}
-                    fontWeight={'500'}
-                >
-                    {ABOUT.summary}
-                </Text>
-            </Box>
+                    <Text
+                        fontSize={{
+                            base: '3xl',
+                            md: '6xl',
+                            lg: '7xl'
+                        }}
+                        fontWeight={'bold'}
+                    >
+                        {ABOUT.name}
+                    </Text>
+                    <Text
+                        fontSize={{
+                            base: 'md',
+                            md: '1rem',
+                            lg: '1rem',
+                            xl: '1.25rem'
+                        }}
+                    >
+                        {ABOUT.note}
+                    </Text>
+                </Box>
+                <Box textAlign={'center'}>
+                    <Text
+                        fontSize={{
+                            base: 'md',
+                            md: '1rem',
+                            lg: '1rem',
+                            xl: '1.25rem'
+                        }}
+                        fontWeight={'500'}
+                    >
+                        {ABOUT.summary}
+                    </Text>
+                </Box>
+            </Container>
         </Box>
     )
 }
