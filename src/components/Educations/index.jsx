a6e753220fc917665818beb11d3896f07613612c
rikzan fernanda <fernandarikzan@gmail.com>
2023-08-17 22:45:00 +0700

    rewrite education section

--- src/components/Educations/index.jsx
+++ src/components/Educations/index.jsx
@@ -1,46 +1,112 @@
-import { Box, Container, Flex, Heading } from '@chakra-ui/react'
+import { Box, Container, Heading, Image, ListItem, SimpleGrid, Text, UnorderedList } from '@chakra-ui/react'
 import React from 'react'
 import education from '../../constants/education'
-import EducationCard from './components/EducationCard'
+import BgEducation from '../../assets/images/bg-education.jpg'
+import IconEducation from '../../assets/images/education.gif'
 
 const Educations = () => {
     return (
         <Box
             py={{
-                base: 8,
+                base: 12,
                 md: 16,
                 lg: 24
             }}
             backgroundColor={'gray.100'}
+            backgroundImage={BgEducation}
+            bgSize={'cover'}
+            bgPosition={'center'}
+            bgAttachment={'fixed'}
         >
             <Container maxW={'container.xl'}>
-                <Heading
-                    fontSize={{
-                        base: '1.25rem',
-                        md: '2rem'
-                    }}
-                    textAlign={{
-                        base: 'center'
-                    }}
-                    mb={{
-                        base: 3,
-                        md: '1.25rem'
-                    }}
-                >
-                    {education.title}
-                </Heading>
+                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={4}>
+                    <Box>
+                        <Heading
+                            fontSize={{
+                                base: '1.25rem',
+                                md: '2rem'
+                            }}
+                            textAlign={{
+                                base: 'center'
+                            }}
+                            mb={{
+                                base: 6,
+                                md: 12
+                            }}
+                            color={'white'}
+                        >
+                            {education.title}
+                        </Heading>
+                        {education.data.map((item, i) => {
+                            return (
+                                <Box key={i}>
+                                    <Box
+                                        mb={6}
+                                        color={'white'}
+                                    >
+                                        <Heading
+                                            fontSize={{
+                                                base: '1rem',
+                                                lg: '1.5rem'
+                                            }}
+                                            mb={{
+                                                base: 1
+                                            }}
+                                        >
+                                            {item.nama}
+                                        </Heading>
+                                        <Text
+                                            fontSize={{
+                                                base: '.85rem',
+                                                lg: '1rem'
+                                            }}
+                                        >
+                                            {item.study} | {item.date.start} - {item.date.end}
+                                        </Text>
+                                    </Box>
+                                    <Box>
+                                        <UnorderedList
+                                            fontSize={{
+                                                base: '.85rem',
+                                                lg: '1rem'
+                                            }}
+                                            color={'white'}
+                                            fontWeight={'500'}
+                                        >
+                                            {item.activities.map((activity, i) => (
+                                                <ListItem key={i}>{activity}</ListItem>
+                                            ))}
+                                        </UnorderedList>
+                                    </Box>
+                                </Box>
+                            )
+                        })}
+                    </Box>
 
-                <Flex
-                    justifyContent={'center'}
-                    alignItems={'center'}
-                    direction={{
-                        base: 'column'
-                    }}
-                >
-                    {education.data.map((item, i) => {
-                        return <EducationCard key={i} item={item} />
-                    })}
-                </Flex>
+                    <Box
+                        w={{
+                            base: 'auto',
+                        }}
+                        h={{
+                            base: 350
+                        }}
+                        display={{
+                            base: 'none',
+                            md: 'flex'
+                        }}
+                        justifyContent={'center'}
+                        alignItems={'center'}
+                    >
+                        <Image
+                            src={IconEducation}
+                            w={{
+                                base: '100%'
+                            }}
+                            h={'100%'}
+                            objectFit={'contain'}
+                        />
+                    </Box>
+                </SimpleGrid>
             </Container>
         </Box>
     )
--- src/constants/education.js
+++ src/constants/education.js
@@ -4,13 +4,21 @@ const education = {
         {
             nama: 'Dian Nuswantoro University',
             study: 'Bachelor of Computer Science',
+            gpa: '3.74',
             description:
                 'Activities and societies: Focus on computer science, create web and mobile applications, become a laboratory assistant, and administrator of the Dian Nuswantoro Club.',
             date: {
                 start: '2019',
                 end: '2023'
             },
-            activities: ['Learn programming', 'Learn how to build application']
+            activities: [
+                'Completed web training through Dian Nuswantoro Computer CLub',
+                'Became a laboratory assistant for the programming class at Dian Nuswantoro Programming Laboratory',
+                'Became an Activity Manager at Dian Nuswantoro Computer Club (DNCC) for the 2020-2021 period',
+                'Became a committee member of the Dinacom 2021 event',
+                'Completed "Studi Independen" Kampus Merdeka program for 1 semester with role as a Frontend Engineer',
+                'Completed Certified Internship Kampus Merdeka program for 1 semester with role as a Fullstack Developer'
+            ]
         }
     ]
 }
